662318d5aae798b305a8e663851d88f18092d89b
hari vikas gavini <vikashari8328@gmail.com>
2026-04-02 15:30:47 +0530

    Add files via upload

--- utils/role_checker.py
+++ utils/role_checker.py
@@ -1,1 +1,11 @@
+from fastapi import Header, HTTPException, Depends
 
+def get_role(role: str = Header(...)):
+    return role
+
+def require_role(required_role: str):
+    def checker(role: str = Depends(get_role)):
+        hierarchy = ["viewer", "analyst", "admin"]
+        if hierarchy.index(role) < hierarchy.index(required_role):
+            raise HTTPException(status_code=403, detail="Not enough permissions")
+    return checker
